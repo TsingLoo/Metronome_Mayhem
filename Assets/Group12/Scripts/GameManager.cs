061740a2ff1df80c0bab84ffa7743a212c5108f7
chj259 <chjessie@seas.upenn.edu>
2024-09-29 18:24:15 -0400

    Added Healthbar

--- Assets/Group12/Scripts/GameManager.cs
+++ Assets/Group12/Scripts/GameManager.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 namespace Group12
 {
+    
     public class GameManager : MonoBehaviour
     {
+        public Health MyHealth; 
         private int totalNotes;
         [SerializeField] private int missedNum, perfectNum, excellentNum, greatNum, okNum;
         private int combo;
@@ -23,41 +25,59 @@ namespace Group12
             {
                 Instance = this;
             }
+            combo = 0;
         }
 
         public void noNote()
         {
             // remove combo, decrease health
+            MyHealth.ChangeHealth(-5);
+            //combo = 0;
         }
 
         public void missedNote()
         {
             // add a Miss, remove combo, decrease health
             missedNum++;
+            MyHealth.ChangeHealth(-5);
+            //combo = 0;
+
         }
 
         public void okNote()
         {
             // add an Ok, remove combo, decrease health
             okNum++;
+            MyHealth.ChangeHealth(-3);
+            combo++;
+            Debug.Log("Combo: " + combo);
         }
 
         public void greatNote()
         {
             // add a Great, remove combo, decrease health
             greatNum++;
+            MyHealth.ChangeHealth(-1);
+            combo++;
+            Debug.Log("Combo: " + combo);
         }
 
         public void excellentNote()
         {
             // add an Excellent, decrease health, increase combo
             excellentNum++;
+            MyHealth.ChangeHealth(1);
+            combo++;
+            Debug.Log("Combo: " + combo);
         }
 
         public void perfectNote()
         {
             // add a Perfect, increase combo\
             perfectNum++;
+            MyHealth.ChangeHealth(5);
+            combo++;
+            Debug.Log("Combo: " + combo);
         }
     }
 }
---
+++ Assets/Group12/Scripts/Health.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Slider slider;
+    public int maxHealth = 100;
+    private int curHealth;
+    public static Health Instance { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+    }
+
+    public void ChangeHealth(int change)
+    {
+        curHealth += change;
+        if (curHealth > maxHealth)
+        {
+            curHealth = maxHealth;
+        } 
+        else if (curHealth < 0)
+        {
+            //lose the game
+            curHealth = 0;
+        }
+
+        slider.value = curHealth;
+
+    }
+    /*
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeHealth(-10);
+            Debug.Log("Health: " + curHealth);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ChangeHealth(10);
+            Debug.Log("Health: " + curHealth);
+        }
+    }*/
+}
